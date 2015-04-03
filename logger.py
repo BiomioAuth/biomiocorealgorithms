103aa3dea3484fb6109b96834c9d06e8c1c5a9e7
Vitalius Parubochyi <vitalius.parubochyi@vakoms.com.ua>
2015-04-03 13:37:38 +0300

    Added Logger class

--- logger.py
+++ logger.py
@@ -10,3 +10,28 @@ logging.basicConfig(
 # handler.setFormatter(logging.Formatter("%(levelname)-1s" + '\t' + " [%(asctime)s]" + '\t' + " %(message)s"))
 # logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
+
+
+class Logger:
+    def __init__(self):
+        self._isLogging = True
+        self._level = 0
+        self._log = []
+
+    def useLogging(self, use):
+        self._isLogging = use
+
+    def level(self, l):
+        self._level = l
+        for i in range(0, l):
+            self._log.append("")
+
+    def debug(self, level, message):
+        if self._isLogging:
+            logger.log(message)
+        if self._level > 0:
+            log = self._log[level]
+            if not log:
+                log = ""
+            log += message + "\n"
+            self._log[level] = log
