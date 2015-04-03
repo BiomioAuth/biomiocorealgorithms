d880be5bd398ed6dff7be28c189f6be6306c600d
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2015-04-03 14:55:00 +0300

    Implemented changes to algo package, changed algo job , added probe results listener, changed probeauthbeckend so algorithm is executed asynchronously

---
+++ algo_job_processor.py
@@ -0,0 +1,19 @@
+import logging
+from rq import Queue, use_connection
+from redis import Redis
+
+logger = logging.getLogger(__name__)
+
+use_connection()
+q = Queue(connection=Redis())
+
+
+def run_algo_job(job_to_run, **kwargs):
+    """
+        Put job in worker's queue.
+    :param job_to_run:
+    :param kwargs:
+    """
+    logger.info('Running job - %s' % str(job_to_run))
+    logger.info(kwargs)
+    q.enqueue(job_to_run, **kwargs)
--- algo_jobs.py
+++ algo_jobs.py
@@ -1,13 +1,20 @@
-__author__ = 'vitalius.parubochyi'
-
+import shutil
+import tempfile
+from biomio.constants import REDIS_PROBE_RESULT_KEY
+from biomio.protocol.storage.redis_storage import RedisStorage
 from algorithms_interface import AlgorithmsInterface
 import logging
-
+import os
+import binascii
+import json
 
 logger = logging.getLogger(__name__)
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+ALGO_DB_PATH = os.path.join(APP_ROOT, 'data')
 
-def verification_job(**kwargs):
+
+def verification_job(callback_code, data, fingerprint):
     """
     Creating verification job
 
@@ -19,63 +26,146 @@ def verification_job(**kwargs):
             'data'      Absolute path to image for verification
             'database'  BLOB data of user, with userID, for verification algorithm algoID
     """
-    result = False
-    logger.info('Running verification for user - %s, with given parameters - %s' % (kwargs['userID'], kwargs))
-    try:
-        record = AlgorithmsInterface.verification(**kwargs)
-        if record['status'] == "result":
-            # record = dictionary:
-            #      key          value
-            #      'status'     "result"
-            #      'result'     bool value: True is verification successfully, otherwise False
-            #      'userID'     Unique user identificator
-            #
-            # Need save to redis
-            result = record.get('result', False)
-            pass
-        elif record['status'] == "data_request":
-            # record = dictionary:
-            #      key          value
-            #      'status'     "data_request"
-            #      'userID'     Unique user identificator
-            #      'algoID'     Unique algorithm identificator
-            #
-            # Need save to redis as data request (for this we can use this dictionary)
-            pass
-        elif record['status'] == "update":
-            # record = dictionary:
-            #      key          value
-            #      'status'     "update"
-            #      'userID'     Unique user identificator
-            #      'algoID'     Unique algorithm identificator
-            #      'database'   BLOB data of user, with userID, for verification algorithm algoID
-            #
-            # Need update record in algorithms database or create record for user userID and algorithm
-            # algoID if it doesn't exists
-            pass
-        elif record['status'] == "error":
-            print record['status'], record['type'], record['details']
-            # record = dictionary:
-            #      key          value
-            #      'status'     "error"
-            #      'type'       Type of error
-            #      'userID'     Unique user identificator
-            #      'algoID'     Unique algorithm identificator
-            #      'details'    Error details dictionary
-            #
-            # Algorithm can have three types of errors:
-            #       "Algorithm settings are empty"
-            #        in this case fields 'userID', 'algoID', 'details' are empty
-            #       "Invalid algorithm settings"
-            #        in this case 'details' dictionary has following structure:
-            #               key         value
-            #               'params'    Parameters key ('data')
-            #               'message'   Error message (for example "File <path> doesn't exists")
-            #       "Internal algorithm error"
-            # Need save to redis
-            pass
-        logger.info('Verification was run for user - %s, with given parameters - %s' % (kwargs['userID'], kwargs))
-    except Exception as e:
-        logger.exception(msg=str(e))
+    result = True
+    settings = dict()
+    settings['algoID'] = "001002"
+    settings['userID'] = "0000000000000"
+    settings['database'] = load_sources(os.path.join(ALGO_DB_PATH, "%s.json" % fingerprint))
+    logger.info('Running verification for user - %s, with given parameters - %s' % (settings['userID'], settings))
+    for sample in data:
+        if result:
+            break
+        try:
+            temp_image_path = tempfile.mkdtemp(dir=APP_ROOT)
+            fd, temp_image = tempfile.mkstemp(dir=temp_image_path)
+            os.close(fd)
+            photo_data = binascii.a2b_base64(str(sample))
+            with open(temp_image, 'wb') as f:
+                f.write(photo_data)
+            settings['data'] = temp_image
+            record = AlgorithmsInterface.verification(**settings)
+            if record['status'] == "result":
+                # record = dictionary:
+                #      key          value
+                #      'status'     "result"
+                #      'result'     bool value: True is verification successfully, otherwise False
+                #      'userID'     Unique user identificator
+                #
+                # Need save to redis
+                result = record.get('result', False)
+            elif record['status'] == "data_request":
+                # record = dictionary:
+                #      key          value
+                #      'status'     "data_request"
+                #      'userID'     Unique user identificator
+                #      'algoID'     Unique algorithm identificator
+                #
+                # Need save to redis as data request (for this we can use this dictionary)
+                pass
+            elif record['status'] == "update":
+                # record = dictionary:
+                #      key          value
+                #      'status'     "update"
+                #      'userID'     Unique user identificator
+                #      'algoID'     Unique algorithm identificator
+                #      'database'   BLOB data of user, with userID, for verification algorithm algoID
+                #
+                # Need update record in algorithms database or create record for user userID and algorithm
+                # algoID if it doesn't exists
+                pass
+            elif record['status'] == "error":
+                print record['status'], record['type'], record['details']
+                # record = dictionary:
+                #      key          value
+                #      'status'     "error"
+                #      'type'       Type of error
+                #      'userID'     Unique user identificator
+                #      'algoID'     Unique algorithm identificator
+                #      'details'    Error details dictionary
+                #
+                # Algorithm can have three types of errors:
+                #       "Algorithm settings are empty"
+                #        in this case fields 'userID', 'algoID', 'details' are empty
+                #       "Invalid algorithm settings"
+                #        in this case 'details' dictionary has following structure:
+                #               key         value
+                #               'params'    Parameters key ('data')
+                #               'message'   Error message (for example "File <path> doesn't exists")
+                #       "Internal algorithm error"
+                # Need save to redis
+                pass
+        except Exception as e:
+            logger.exception(e)
+        finally:
+            shutil.rmtree(temp_image_path)
+    RedisStorage.persistence_instance().store_data(key=REDIS_PROBE_RESULT_KEY % callback_code, result=result)
+    logger.info('Verification was run for user - %s, with given parameters - %s' % (settings['userID'], settings))
+    # try:
+    #     record = AlgorithmsInterface.verification(**kwargs)
+    #     if record['status'] == "result":
+    #         # record = dictionary:
+    #         #      key          value
+    #         #      'status'     "result"
+    #         #      'result'     bool value: True is verification successfully, otherwise False
+    #         #      'userID'     Unique user identificator
+    #         #
+    #         # Need save to redis
+    #         result = record.get('result', False)
+    #         RedisStorage.persistence_instance().store_data(key=REDIS_PROBE_RESULT_KEY % callback_code, result=result)
+    #         pass
+    #     elif record['status'] == "data_request":
+    #         # record = dictionary:
+    #         #      key          value
+    #         #      'status'     "data_request"
+    #         #      'userID'     Unique user identificator
+    #         #      'algoID'     Unique algorithm identificator
+    #         #
+    #         # Need save to redis as data request (for this we can use this dictionary)
+    #         pass
+    #     elif record['status'] == "update":
+    #         # record = dictionary:
+    #         #      key          value
+    #         #      'status'     "update"
+    #         #      'userID'     Unique user identificator
+    #         #      'algoID'     Unique algorithm identificator
+    #         #      'database'   BLOB data of user, with userID, for verification algorithm algoID
+    #         #
+    #         # Need update record in algorithms database or create record for user userID and algorithm
+    #         # algoID if it doesn't exists
+    #         pass
+    #     elif record['status'] == "error":
+    #         print record['status'], record['type'], record['details']
+    #         # record = dictionary:
+    #         #      key          value
+    #         #      'status'     "error"
+    #         #      'type'       Type of error
+    #         #      'userID'     Unique user identificator
+    #         #      'algoID'     Unique algorithm identificator
+    #         #      'details'    Error details dictionary
+    #         #
+    #         # Algorithm can have three types of errors:
+    #         #       "Algorithm settings are empty"
+    #         #        in this case fields 'userID', 'algoID', 'details' are empty
+    #         #       "Invalid algorithm settings"
+    #         #        in this case 'details' dictionary has following structure:
+    #         #               key         value
+    #         #               'params'    Parameters key ('data')
+    #         #               'message'   Error message (for example "File <path> doesn't exists")
+    #         #       "Internal algorithm error"
+    #         # Need save to redis
+    #         pass
+    #     logger.info('Verification was run for user - %s, with given parameters - %s' % (kwargs['userID'], kwargs))
+    # except Exception as e:
+    #     logger.exception(msg=str(e))
+    #
+    # return result
 
-    return result
+
+def load_sources(path):
+    if len(path):
+        if not os.path.exists(path):
+            return dict()
+        with open(path, "r") as data_file:
+            source = json.load(data_file)
+            return source
+    return dict()
