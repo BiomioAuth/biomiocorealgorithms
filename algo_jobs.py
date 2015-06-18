e82eeaff303c094009882af672e0b3af40eeacff
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2015-06-18 19:22:09 +0300

    Moved functionality that tells AI that training started/finished from storage_jobs to algo training job.

--- algo_jobs.py
+++ algo_jobs.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import
 import StringIO
+import base64
+import requests
 import shutil
 import tempfile
 import cPickle
+from requests.exceptions import HTTPError
 from biomio.constants import REDIS_PROBE_RESULT_KEY, REDIS_RESULTS_COUNTER_KEY, REDIS_PARTIAL_RESULTS_KEY, \
-    TRAINING_DATA_TABLE_CLASS_NAME, REDIS_JOB_RESULTS_ERROR
+    TRAINING_DATA_TABLE_CLASS_NAME, REDIS_JOB_RESULTS_ERROR, REST_REGISTER_BIOMETRICS, get_ai_training_response
 from biomio.mysql_storage.mysql_data_store_interface import MySQLDataStoreInterface
 from biomio.protocol.storage.redis_storage import RedisStorage
 from biomio.algorithms.algorithms_interface import AlgorithmsInterface
@@ -168,7 +171,7 @@ def store_test_photo_helper(image_paths):
         shutil.copyfile(path, os.path.join(TEST_PHOTO_PATH, os.path.basename(path)))
 
 
-def training_job(images, fingerprint, settings, callback_code):
+def training_job(images, fingerprint, settings, callback_code, try_type, ai_code):
     """
         Runs education for given user with given array of images.
     :param images: array of images to run verification on.
@@ -178,6 +181,25 @@ def training_job(images, fingerprint, settings, callback_code):
     """
     worker_logger.info('Running training for user - %s, with given parameters - %s' % (settings.get('userID'),
                                                                                        settings))
+    ai_response_type = dict()
+    try:
+
+        worker_logger.info('Telling AI that we are starting training with code - %s' % ai_code)
+        ai_response_type.update({'status': 'in-progress'})
+        response_type = base64.b64encode(json.dumps(ai_response_type))
+        register_biometrics_url = settings.ai_rest_url % (REST_REGISTER_BIOMETRICS % (ai_code, response_type))
+        response = requests.post(register_biometrics_url)
+        try:
+            response.raise_for_status()
+            worker_logger.info('AI should now that training started with code - %s and response type - %s' %
+                               (ai_code, response_type))
+        except HTTPError as e:
+            worker_logger.exception(e)
+            worker_logger.exception('Failed to tell AI that training started, reason - %s' % response.reason)
+    except Exception as e:
+        worker_logger.error('Failed to build rest request to AI - %s' % str(e))
+        worker_logger.exception(e)
+    ai_response_type.update({'status': 'verified'})
     result = False
     settings.update({'action': 'education'})
     temp_image_path = tempfile.mkdtemp(dir=APP_ROOT)
@@ -206,7 +228,6 @@ def training_job(images, fingerprint, settings, callback_code):
             #
             # Need update record in algorithms database or create record for user userID and algorithm
             # algoID if it doesn't exists
-            database = algo_result.get('database')
             database_path = os.path.join(ALGO_DB_PATH, "%s.json" % fingerprint)
             database = algo_result.get('database', None)
             if database is not None:
@@ -227,6 +248,7 @@ def training_job(images, fingerprint, settings, callback_code):
             worker_logger.exception('Error during education - %s, %s, %s' % (algo_result.get('status'),
                                                                              algo_result.get('type'),
                                                                              algo_result.get('details')))
+            ai_response_type.update({'status': 'error'})
             # record = dictionary:
             # key          value
             #      'status'     "error"
@@ -251,4 +273,22 @@ def training_job(images, fingerprint, settings, callback_code):
     finally:
         RedisStorage.persistence_instance().store_data(key=REDIS_PROBE_RESULT_KEY % callback_code, result=result)
         shutil.rmtree(temp_image_path)
+        if result:
+            ai_response_type.update(get_ai_training_response(try_type))
+        try:
+            worker_logger.info('Telling AI that training is finished with code - %s and result - %s' %
+                               (ai_code, result))
+            response_type = base64.b64encode(json.dumps(ai_response_type))
+            register_biometrics_url = settings.ai_rest_url % (REST_REGISTER_BIOMETRICS % (ai_code, response_type))
+            response = requests.post(register_biometrics_url)
+            try:
+                response.raise_for_status()
+                worker_logger.info('AI should now that training is finished with code - %s and response type - %s' %
+                                   (ai_code, response_type))
+            except HTTPError as e:
+                worker_logger.exception(e)
+                worker_logger.exception('Failed to tell AI that training is finished, reason - %s' % response.reason)
+        except Exception as e:
+            worker_logger.error('Failed to build rest request to AI - %s' % str(e))
+            worker_logger.exception(e)
     worker_logger.info('training finished for user - %s, with result - %s' % (settings.get('userID'), result))
