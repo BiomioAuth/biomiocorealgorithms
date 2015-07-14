1a72bbb8db6b93122effebc99bb19e971a171b34
vitaliusparubochyi <vitalius.parubochyi@vakoms.com.ua>
2015-07-14 19:13:58 +0300

    Code refactoring. Removed unused parameters for methods

--- algorithms/cascades/rectmerge.py
+++ algorithms/cascades/rectmerge.py
@@ -1,37 +1,34 @@
 
-def mergeRectangles(rects, general=True, convex=True):
+def mergeRectangles(rects):
     if len(rects) == 0:
         return rects
     if len(rects) == 1:
         return rects[0]
     half = len(rects) / 2
-    left = mergeRectangles(rects[:half], general, convex)
-    right = mergeRectangles(rects[half:], general, convex)
-    return _mergeRect(left, right, general, convex)
+    left = mergeRectangles(rects[:half])
+    right = mergeRectangles(rects[half:])
+    return _mergeRect(left, right)
 
 
-def _mergeRect(left, right, general, convex):
-    if convex:
-        if general:
-            p_left = left[0]
-            if p_left > right[0]:
-                p_left = right[0]
-            p_top = left[1]
-            if p_top > right[1]:
-                p_top = right[1]
-            p_right = left[0] + left[2]
-            if p_right < right[0] + right[2]:
-                p_right = right[0] + right[2]
-            p_bottom = left[1] + left[3]
-            if p_bottom < right[1] + right[3]:
-                p_bottom = right[1] + right[3]
-            return [p_left, p_top, p_right - p_left, p_bottom - p_top]
-    return [0, 0, 0, 0]
+def _mergeRect(left, right):
+    p_left = left[0]
+    if p_left > right[0]:
+        p_left = right[0]
+    p_top = left[1]
+    if p_top > right[1]:
+        p_top = right[1]
+    p_right = left[0] + left[2]
+    if p_right < right[0] + right[2]:
+        p_right = right[0] + right[2]
+    p_bottom = left[1] + left[3]
+    if p_bottom < right[1] + right[3]:
+        p_bottom = right[1] + right[3]
+    return [p_left, p_top, p_right - p_left, p_bottom - p_top]
 
 
 def main():
     rects = [[0, 0, 4, 2], [1, 1, 2, 2], [2, 2, 4, 2], [7, 1, 2, 2]]
-    print mergeRectangles(rects, True, True)
+    print mergeRectangles(rects)
 
 if __name__ == '__main__':
     main()
