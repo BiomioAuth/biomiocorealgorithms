94fe91ae665c4b457336a72e5fd289ba417102c6
vitaliusparubochyi <vitalius.parubochyi@vakoms.com.ua>
2015-11-26 19:01:03 +0200

    Added interface for algorithm processes

---
+++ recognition/interfaces.py
@@ -0,0 +1,52 @@
+# from logger import logger
+from logger import algo_logger as logger
+
+
+class AlgorithmProcessInterface:
+    def __init__(self):
+        self._classname = "AlgorithmProcessInterface"
+        self._error_process = None
+
+    def set_error_handler_process(self, process):
+        self._error_process = process
+
+    def handler(self, result):
+        raise NotImplementedError
+
+    def job(self, callback_code, **kwargs):
+        raise NotImplementedError
+
+    def process(self, **kwargs):
+        raise NotImplementedError
+
+    def run(self, worker, kwargs_list_for_results_gatherer=None, **kwargs):
+        if worker is not None:
+            worker.run_job(self.job, callback=self.handler,
+                           kwargs_list_for_results_gatherer=kwargs_list_for_results_gatherer, **kwargs)
+
+    def _handler_logger_info(self, result):
+        logger.debug("+++++++++++++++++++++++++++++++++++")
+        logger.debug("%s::Handler", self._classname)
+        logger.debug(result)
+        logger.debug("+++++++++++++++++++++++++++++++++++")
+
+    def _job_logger_info(self, **kwargs):
+        logger.debug("-----------------------------------")
+        logger.debug("%s::Job", self._classname)
+        logger.debug(kwargs)
+        logger.debug("-----------------------------------")
+
+    def _process_logger_info(self, **kwargs):
+        logger.debug("===================================")
+        logger.debug("%s::Process", self._classname)
+        logger.debug(kwargs)
+        logger.debug("===================================")
+
+
+class AlgorithmInterface:
+
+    def training(self, **kwargs):
+        raise NotImplementedError
+
+    def apply(self, **kwargs):
+        raise NotImplementedError
