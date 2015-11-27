03034ab0f0d6b03bae144c0bac4f4d58f9c4ef10
vitaliusparubochyi <vitalius.parubochyi@vakoms.com.ua>
2015-11-27 12:59:18 +0200

    Added few effects for cvtools

--- cvtools/effects.py
+++ cvtools/effects.py
@@ -40,3 +40,20 @@ def grayscaleAndEqualize(image):
     """
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return cv2.equalizeHist(gray)
+
+
+def binarization(image):
+    res, binary = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY)
+    return binary
+
+
+def gaussianBlurring(image, kernel=(5, 5)):
+    return cv2.GaussianBlur(image, kernel, 0)
+
+
+def rotate90(image):
+    rows = image.shape[0]
+    cols = image.shape[1]
+    M = cv2.getRotationMatrix2D((cols/2.0, cols/2.0), 90, 1)
+    img = cv2.warpAffine(image, M, (rows, cols))
+    return img
