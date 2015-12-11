1107c5f364aa970f1bb933c886769dcb8514da56
vitaliusparubochyi <vitalius.parubochyi@vakoms.com.ua>
2015-12-11 12:26:26 +0200

    Fixed imports in keypoints module

--- recognition/keypoints.py
+++ recognition/keypoints.py
@@ -1,39 +1,8 @@
 from __future__ import absolute_import
-from biomio.algorithms.features import (constructDetector, constructSettings, BRISKDetectorType)
-from biomio.algorithms.features import (FeatureDetector)
+from biomio.algorithms.features.features import (FeatureDetector)
+from biomio.algorithms.recognition.kodsettings import KODSettings
+from biomio.algorithms.features import (constructDetector)
 import logger
-
-
-class KODSettings:
-    """
-    Keypoints Object Detector's Settings class
-    """
-    neighbours_distance = 1.0
-    detector_type = BRISKDetectorType
-    settings = None
-    probability = 25.0
-
-    def exportSettings(self):
-        return {
-            'Neighbours Distance': self.neighbours_distance,
-            'Probability': self.probability,
-            'Detector Type': self.detector_type,
-            'Detector Settings': self.settings.exportSettings()
-        }
-
-    def importSettings(self, settings):
-        self.neighbours_distance = settings['Neighbours Distance']
-        self.probability = settings['Probability']
-        self.detector_type = settings.get('Detector Type')
-        self.settings = constructSettings(self.detector_type)
-        self.settings.importSettings(settings.get('Detector Settings', dict()))
-
-    def dump(self):
-        logger.algo_logger.info('Keypoints Objects Detectors Settings')
-        logger.algo_logger.info('Neighbours Distance: %f' % self.neighbours_distance)
-        logger.algo_logger.info('Probability: %f' % self.probability)
-        logger.algo_logger.info('Detector Type: %s' % self.detector_type)
-        self.settings.dump()
 
 
 def identifying(fn):
