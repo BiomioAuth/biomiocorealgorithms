508e67fcdbb6d09ed7cf0157c42ae89df87dcbce
vitaliusparubochyi <vitalius.parubochyi@vakoms.com.ua>
2016-06-17 17:40:29 +0300

    Fixed checking of database items

--- openface/openface_simple_dist_estimate.py
+++ openface/openface_simple_dist_estimate.py
@@ -39,7 +39,7 @@ class OpenFaceSimpleDistanceEstimation(IAlgorithm):
             return {'result': 0}
         avg = 0
         for item in database.get('data', []):
-            if len(item) <= 0:
+            if len(item['rep']) <= 0:
                 continue
             avg += distance.euclidean(tdata['rep'], item['rep'])
         avg /= len(database.get('data', []))
