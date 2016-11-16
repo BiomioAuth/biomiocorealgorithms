b0674cc149fc247eb45f88d25aae50ca2a353fbd
Vitalius <eddragonwolf@gmail.com>
2016-11-16 15:21:50 +0200

    Added unified package interface for flows package

--- flows/__init__.py
+++ flows/__init__.py
@@ -1,8 +1,13 @@
+# cascades module
+from cascades.cascades_face_detect_algorithm import CascadesFaceDetectionAlgorithm
+from cascades.rotation_detect_algorithm import RotationDetectionAlgorithm
+
+# general module
+from general.base import IAlgorithm, AlgorithmFlow, LinearAlgorithmFlow
+from general.first_success_flow import FirstSuccessFlow
+
+# xopenface module
 from xopenface.dlib_face_detect_algorithm import DLibFaceDetectionAlgorithm, INNER_EYES_AND_BOTTOM_LIP, \
     OUTER_EYES_AND_NOSE, DLIB_PREDICTOR_V1, DLIB_PREDICTOR_V2
 from xopenface.openface_simple_dist_estimate import OpenFaceSimpleDistanceEstimation
-from cascades.cascades_face_detect_algorithm import CascadesFaceDetectionAlgorithm
-from cascades.rotation_detect_algorithm import RotationDetectionAlgorithm
 from xopenface.openface_data_rep import OpenFaceDataRepresentation
-from general.first_success_flow import FirstSuccessFlow
-from general.base import IAlgorithm, AlgorithmFlow
